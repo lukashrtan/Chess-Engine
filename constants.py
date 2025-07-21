0743df0d4036d480f9f55750613eb27474f87502
metodejsamal <metodej.samal@gamil.com>
2025-07-21 11:22:50 +0200

    rock done

--- constants.py
+++ constants.py
@@ -21,15 +21,29 @@ class Moves:
     def Rock(self, board, position):
         positions = []
         for x in range(1, 8):
-            if position + x * 8 < 64:
+            if position + x * 8 < 64 and self.board[position + x * 8] == 0:
                 positions.append(position + x * 8)
+            else:
+                break
+
         for x in range(1, 8):
-            if position - x * 8 > 64:
+            if position - x * 8 > 64 and self.board[position - x * 8] == 0:
                 positions.append(position + x * 8)
+            else:
+                break
+
         for x in range(1, 8 - position % 8):
-            positions.append(position + x)
+            if self.board[position + x] == 0:
+                positions.append(position + x)
+            else:
+                break
+
         for x in range(1, position % 8 + 1):
-            positions.append(position - x)
+            if self.board[position - x] == 0:
+                positions.append(position - x)
+            else:
+                break
+        return positions
 
 
 
